9919a83e9f4d0ca9dd39beae8647489838b1598e
Gustavo <gustavo38000@gmail.com>
2024-09-11 11:02:43 -0300

    Adicionando animação ao slide

---
+++ src/javascript/slide.js
@@ -0,0 +1,39 @@
+const leftBtn = document.querySelector("#left-arrow");
+const rightBtn = document.querySelector("#right-arrow");
+const slideImg = document.querySelectorAll(".slide-element");
+
+console.log(slideImg.length);
+
+let slideIndex = 0;
+
+rightBtn.addEventListener("click", () => {
+  slideIndex++;
+
+  if (slideIndex >= slideImg.length - 2) {
+    slideIndex = 0;
+  }
+
+  const slideWidth = 311;
+  const offset = slideIndex * slideWidth;
+
+  slideImg.forEach((img) => {
+    img.style.transform = `translateX(-${offset}px)`;
+    img.style.transition = `all 1s`;
+  });
+});
+
+leftBtn.addEventListener("click", () => {
+  slideIndex--;
+
+  if (slideIndex == 0) {
+    slideIndex = 0;
+  }
+
+  const slideWidth = 311;
+  const offset = slideIndex * slideWidth;
+
+  slideImg.forEach((img) => {
+    img.style.transform = `translateX(-${offset}px)`;
+    img.style.transition = `all 1s`;
+  });
+});
